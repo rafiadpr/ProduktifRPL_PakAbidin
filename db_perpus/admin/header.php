74e26523251f95c79eac9fe9f7d5e891aa21e038
rafiadpr <99937854+rafiadpr@users.noreply.github.com>
2022-09-22 06:40:53 +0700

    Delete db_perpus/admin directory

--- db_perpus/admin/header.php
+++
@@ -1,7 +0,0 @@
-<?php 
-session_start();
-    if($_SESSION['status_login']!=true){
-        header('location: login.php');
-    }
-?>
-<h2 align='center'>Selamat datang Admin di website Perpus Online.</h2>
--- db_perpus/admin/koneksi.php
+++
@@ -1,7 +0,0 @@
-<?php
-    $host="localhost";
-    $user="root";
-    $pass="";
-    $database="db_perpus";
-    $conn=mysqli_connect($host,$user,$pass,$database);
-?>
