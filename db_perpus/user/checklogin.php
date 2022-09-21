755201129c35e70a033efe017103c5143a7b3901
rafiadpr <99937854+rafiadpr@users.noreply.github.com>
2022-09-22 06:41:11 +0700

    Delete db_perpus/user directory

--- db_perpus/user/checklogin.php
+++
@@ -1,6 +0,0 @@
-<?php
-    session_start();
-    if($_SESSION['proses_login.php']){
-        header('location: db_perpus/login.php');
-    }
-?>
--- db_perpus/user/header.php
+++
@@ -1,6 +0,0 @@
-<?php 
-session_start();
-    if($_SESSION['status_login']!=true){
-        header('location: login.php');
-    }
-?>
